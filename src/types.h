5c19cfd919e6609a6c14861606c8d13b3b8ed81a
Joji0 <jonathankw2005@gmail.com>
2026-04-27 20:47:51 +0700

    feat: basic types

---
+++ src/types.h
@@ -0,0 +1,50 @@
+#ifndef TYPES_H
+#define TYPES_H
+// Enumeration of basic types
+enum Color { WHITE, BLACK, NUM_COLORS };
+enum PieceType { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, NO_PIECE_TYPE };
+enum Piece
+{
+        W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING,
+        B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING,
+        NO_PIECE
+};
+enum Square
+{
+        A1, B1, C1, D1, E1, F1, G1, H1,
+        A2, B2, C2, D2, E2, F2, G2, H2,
+        A3, B3, C3, D3, E3, F3, G3, H3,
+        A4, B4, C4, D4, E4, F4, G4, H4,
+        A5, B5, C5, D5, E5, F5, G5, H5,
+        A6, B6, C6, D6, E6, F6, G6, H6,
+        A7, B7, C7, D7, E7, F7, G7, H7,
+        A8, B8, C8, D8, E8, F8, G8, H8,
+        NO_SQUARE
+};
+enum File
+{
+        FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H
+};
+enum Rank
+{
+        RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8
+};
+
+// Helper
+Square make_square(File file, Rank rank)
+{
+        int square = rank * 8 + file;
+        return static_cast<Square>(square);
+}
+File file_of(Square square)
+{
+        int file = square % 8;
+        return static_cast<File>(file);
+}
+Rank rank_of(Square square)
+{
+        int rank = square / 8;
+        return static_cast<Rank>(rank);
+}
+// TODO: add make_piece and move encoding
+#endif
